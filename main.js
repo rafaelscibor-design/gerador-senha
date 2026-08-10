f652bfad87bd1a9c4df3df6c90cafd76d1e81dc9
rafaelscibor-design <rafael.scibor@escola.pr.gov.br>
2026-08-10 08:20:13 -0300

    19

---
+++ main.js
@@ -0,0 +1,23 @@
+document. querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numerosenha.textContent = tamanhoSenha;
+
+const botoes = document. querySelector('.parametro_senha__botao');
+botoes[0]onclick = diminuitamanho;
+botoes[1]onclick = diminuitamanho;
+
+numeroSenha. textContent = 5;
+if (tamanhoSenha > 1) {
+   tamanhoSenha tamanhoSenha-1
+}
+const botoes = document.querySelector('.paremetro-senha__bot');
+botoes [0].onclick = diminuitamanho;
+
+function diminuitamanho() {
+    if (tamanhoSenha < 20)
+    tamanhoSenha = tamanhoSenha+1;
+
+numeroSenha = tamanhoSenha;
+}
+const campoSenha
+
